78d2fba6660710535ed82996105a92420eb004d6
baileybingham <baileyebingham@gmail.com>
2026-02-02 12:24:46 -0800

    completing bracelet making script

--- Determining beading pattern from tomst data.R
+++ Determining beading pattern from tomst data.R
@@ -5,11 +5,19 @@
 #########   February 1st, 2026 ####################
 ###################################################
 
-# The data needs to be split into 52 unique units which will be represented by 
-# 52 beads (i.e. if using size 8/0 (3mm)beads, a wrist length of 6.5"). 
+# The data needs to be split into at minimum 52 unique units which will be 
+# represented by 52 beads (i.e. if using size 8/0 (3mm)beads, a wrist length 
+# of 6.5"). 
+
+# If we bin the year by every 3 days, then we would need 121 beads to cover
+# the full year, or 61 to cover 6 months. 
+# So the smallest bracelet size would cover 156 days (6.5"; May 1st-October 3rd), and the largest
+# would cover 228 days (9.5"; March 26-November 8).
+
 
 # The temps should be binned into about 8-10 bins, so that there are no more 
 # than that many colours to bead. 
+
 
 ####################### Download packages  ####################################
 library(tidyverse) #includes ggplot, tidyr, dplyr, etc. 
@@ -101,67 +109,107 @@ ggplot(tomst_daily_avg, aes(x = datetime, y = QHI_mean)) +
 rollavg <-tomst_daily_avg %>%
    mutate(
     # Rolling Average: Smoothes data, but lowers peaks
-    rolling_avg = rollmean(QHI_mean, k = 6, fill = NA, align = "center"),
+    rolling_avg = rollmean(QHI_mean, k = 3, fill = NA, align = "center"),
     # Rolling Max: Preserves the heatwave intensity for the week
-    rolling_max = rollmax(QHI_max, k = 6, fill = NA, align = "center"),
+    rolling_max = rollmax(QHI_max, k = 3, fill = NA, align = "center"),
     # Rolling Min: Captures the coolest dip of the week
-    rolling_min = rollapply(QHI_min, width = 6, FUN = min, fill = NA, align = "center"))
-
-  # Select 52 points (one for each bead)
-rollweekly<- rollavg %>%
-  slice(seq(1, n(), by = 7)) %>%
+    rolling_min = rollapply(QHI_min, width = 7, FUN = min, fill = NA, align = "center"))%>%
   drop_na()
 
-rollavg %>%
-  pivot_longer(cols = c(rolling_avg, rolling_max, rolling_min), 
-               names_to = "Method", values_to = "Temp") %>%
-  ggplot(aes(x = datetime, y = Temp, color = Method)) +
-  geom_line(size = 1) +
-  geom_point() +
-  labs(title = "Bead Pattern Comparison: Avg vs Max",
-       subtitle = "Rolling Max preserves heatwave peaks for better visualization",
-       y = "Degrees (C)") +
-  theme_minimal()
-
-#### Combine TOMST with ECCC to compare 2023 to the 30 year average
-cdata<-cbind(eccc, rollavg)
-
-# graph it
-ggplot(cdata, aes(x = dummydate)) +
+### Graph the visualize rolling average
+ggplot(rollavg, aes(x = datetime, y = rolling_avg)) +
+  geom_line(alpha = 0.5) + 
   geom_hline(yintercept = 0, color = "red", linewidth = 0.5, linetype = "dashed") +
-  #ECCC Ribbon (Grey Background)
-  geom_ribbon(aes(ymin = eccc_min_temp, ymax = eccc_max_temp),fill = "grey", alpha = 0.3) +
-  #ECCC Mean Line
-  geom_line(aes(y = eccc_mean_temp), color = "grey20", size = 0.8) +
-  #QHI Ribbon 
-  geom_ribbon(aes(ymin = QHI_min, ymax = QHI_max), fill = "darkred", alpha = 0.3) +
-  #QHI Mean Line
-  geom_line(aes(y = QHI_mean), color = "darkred", size = 1) +
-  scale_x_date(date_labels = "%b %d", date_breaks = "1 month") +
-  labs(title = "QHI ECCC weather station historical temps compared to 2023 TOMST average, max and min temps",
-       subtitle = "Grey: ECCC (30-yr) | Red: QHI TOMST (2023)",
+  geom_ribbon(aes(ymin = rolling_min, ymax = rolling_max), alpha = 0.2, fill = "grey") +
+  scale_x_date(
+    date_breaks = "1 month",   # Set marks at every month
+    date_labels = "%b %Y"      # Format: %b = Abbr Month, %y = year (e.g., Jan 22)
+  ) +
+  theme_minimal() +
+  labs(title = "QHI average temperature 2023",
+       subtitle = "Grey shaded area shows the extreme temperatures for each day",
        x = "Date",
-       y = "Temperature (°C)") +
-  theme_minimal()
+       y = "Mean Daily Air Temperature") 
+
+  # Bin the year into 3 day sections by rolling average
+rollbinned<- rollavg %>%
+  slice(seq(1, n(), by = 3)) %>%
+  drop_na()
+
+cropbinned <- rollbinned %>%
+  filter(datetime >= "2023-03-26" & datetime <= "2023-11-08")
+
+range(cropbinned$rolling_avg, na.rm = TRUE)
+
+# Define your temperature range
+min_temp <- -16
+max_temp <- 18
+
+# Create 12 bins
+cropbinned$temp_bins <- cut(cropbinned$rolling_avg, 
+                    breaks = seq(min_temp, max_temp, length.out = 13), 
+                    include.lowest = TRUE)
+unique(cropbinned$temp_bins) # view the bins
+
+# Create a 15-color palette from "RdBu" (Red to Blue)
+my_colors <- colorRampPalette(c("darkblue","blue", "white","red", "darkred"))(12)
+
+ggplot(cropbinned, aes(x = datetime, y = 1, fill = temp_bins)) +
+  geom_tile(color = "white", linewidth = 0.1)  +
+  scale_fill_manual(values = my_colors)+ 
+  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
+  labs(title = "2023 growing season", subtitle = "Rolling averages binned every 3 days.", x = "Date", fill = "Temp Range") +
+  theme_minimal()  +
+  theme(
+    axis.title.y = element_blank(),
+    axis.text.y  = element_blank(),
+    axis.text.x  = element_text(angle = 70, vjust = 0.5, size = 6)
+  )
 
 
-# what about with rolling 2023 averages?
-ggplot(cdata, aes(x = dummydate)) +
-  geom_hline(yintercept = 0, color = "red", linewidth = 0.5, linetype = "dashed") +
-  #ECCC Ribbon (Grey Background)
-  geom_ribbon(aes(ymin = eccc_min_temp, ymax = eccc_max_temp),fill = "grey", alpha = 0.3) +
-  #ECCC Mean Line
-  geom_line(aes(y = eccc_mean_temp), color = "grey20", size = 0.8) +
-  #QHI actual extremes Ribbon 
-  geom_ribbon(aes(ymin = QHI_min, ymax = QHI_max), fill = "darkred", alpha = 0.3) +
-  #QHI Mean rolling average Line
-  geom_line(aes(y = rolling_avg), color = "darkred", size = 1) +
-  scale_x_date(date_labels = "%b %d", date_breaks = "1 month") +
-  labs(title = "QHI ECCC weather station historical temps compared to 2023 TOMST rolling average and max and min temps",
-       subtitle = "Grey: ECCC (30-yr) | Red: rolling average and actual max and mins of the TOMST (2023)",
-       x = "Date",
-       y = "Temperature (°C)") +
-  theme_minimal()
+#################################################################################
+#### Let's try it without the rolling average ####
 
-### Should consider comparing the TOMST 2023 data to the ECCC 2023 data if it exists?
+# Bin the year into 3 day sections by rolling average
+bin<- tomst_daily_avg %>%
+  slice(seq(1, n(), by = 3)) %>%
+  drop_na()
+
+crop <- bin %>%
+  filter(datetime >= "2023-03-26" & datetime <= "2023-11-08")
+
+range(crop$QHI_mean, na.rm = TRUE)
+
+# Define your temperature range
+min_temp <- -16
+max_temp <- 19
+
+# Create 13 bins
+crop$temp_bins <- cut(crop$QHI_mean, 
+                            breaks = seq(min_temp, max_temp, length.out = 11), 
+                            include.lowest = TRUE)
+unique(crop$temp_bins) # view the bins
+
+# Create a 10-color palette from "RdBu" (Red to Blue)
+my_colors <- colorRampPalette(c("darkblue","blue", "white","red", "darkred"))(10)
+
+# Create letter labels
+num_bins <- length(levels(crop$temp_bins))
+letter_labels <- paste0(LETTERS[1:num_bins], ": ", levels(crop$temp_bins))
+
+ggplot(crop, aes(x = datetime, y = 1, fill = temp_bins)) +
+  geom_tile(color = "white", linewidth = 0.1)  +
+  scale_fill_manual(values = my_colors, labels = letter_labels)+ 
+  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
+  labs(title = "2023 Growing Season on Qikiqtaruk - Herschel Island",
+       subtitle = "Daily average temperature ranges over 3 day periods on QHI.",
+       x = "Date", 
+       fill = "Bead colour and Temp range") +
+  theme_minimal()  +
+  theme(
+    axis.title.y = element_blank(),
+    axis.text.y  = element_blank(),
+    axis.text.x  = element_text(angle = 70, vjust = 0.5, size = 6),
+    legend.text = element_text(size = 8)
+  )
 
